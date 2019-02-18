135de1529b101a33072d394182ac7dd5d385b190
Yuriel849 <yuriel849sp@gmail.com>
2019-02-18 18:29:28 +0900

    updates on basic CRUD functions and testing

--- Message Board/src/main/java/org/badgers/persistence/BoardMapper_Yuriel.java
+++ Message Board/src/main/java/org/badgers/persistence/BoardMapper_Yuriel.java
@@ -5,19 +5,19 @@ import java.util.List;
 import org.badgers.domain.BoardVO;
 import org.badgers.domain.Criteria;
 
-public interface BoardMapperYuriel {
+public interface BoardMapper_Yuriel {
 
 	// BoardService regist()와 연결된다.
-	public void insert(BoardVO board) throws Exception;
+	public int insert(BoardVO board) throws Exception;
+	// BoardService read()와 연결된다.
+	public BoardVO read(Integer bno) throws Exception;
+	// BoardService modify()와 연결된다.
+	public int update(BoardVO board) throws Exception;
 	
 	// BoardService listAll()와 연결된다.
 	public List<BoardVO> getList() throws Exception;
 	// BoardService listCriteria()와 연결된다.
 	public List<BoardVO> getListWithPaging(Criteria cri) throws Exception;
-	// BoardService read()와 연결된다.
-	public BoardVO read(Integer bno) throws Exception;
-	// BoardService modify()와 연결된다.
-	public int update(BoardVO board) throws Exception;
     // BoardService delete()와 연결된다.
 	public int delete(Integer bno) throws Exception;
 	// BoardService listCount()와 연결된다.
--- Message Board/src/main/java/org/badgers/service/BoardServiceImpl_Yuriel.java
+++ Message Board/src/main/java/org/badgers/service/BoardServiceImpl_Yuriel.java
@@ -5,33 +5,37 @@ import java.util.List;
 
 import org.badgers.domain.BoardVO;
 import org.badgers.domain.Criteria;
-import org.badgers.persistence.BoardMapperYuriel;
+import org.badgers.persistence.BoardMapper_Yuriel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.Setter;
 
 @Service
-public class BoardServiceImplYuriel implements BoardService {
+public class BoardServiceImpl_Yuriel implements BoardService {
 
 	@Setter(onMethod_ = @Autowired)
-	private BoardMapperYuriel mapper;
+	private BoardMapper_Yuriel mapper;
 	
 	// 게시글을 새로 추가한다.
-	public void regist(BoardVO board) throws Exception {
-		mapper.insert(board);
+	public int regist(BoardVO board) throws Exception {
+		return mapper.insert(board);
 	}
 	
 	// 특정 게시글을 읽어온다.
 	public BoardVO read(Integer bno) throws Exception {
-		return new BoardVO();
+		return mapper.read(bno);
 	}
 	
 	// 특정 게시글을 수정한다.
-	public void modify(BoardVO board) throws Exception {}
+	public int modify(BoardVO board) throws Exception {
+		return mapper.update(board);
+	}
 	
 	// 특정 게시글을 삭제한다.
-	public void remove(Integer bno) throws Exception {}
+	public int remove(Integer bno) throws Exception {
+		return 1;
+	}
 	
 	// 게시글 모두를 읽어온다.
 	public List<BoardVO> listAll() throws Exception {
